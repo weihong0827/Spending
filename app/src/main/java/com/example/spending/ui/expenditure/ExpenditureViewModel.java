83c61b90815cda3d54dacddc7d7c63902ea36841
Clement <clementlohck@outlook.com>
2022-05-29 04:19:28 +0800

    Revert "Trying to implement Iggy's piechart while getting data from firebase but I got confused so stopping here and going to implement piechart purely from fixed values first"
    This reverts commit b049cab121832aad4cb1e007dd2ac30c620d8797.

--- app/src/main/java/com/example/spending/ui/expenditure/ExpenditureViewModel.java
+++ app/src/main/java/com/example/spending/ui/expenditure/ExpenditureViewModel.java
@@ -1,7 +1,5 @@
 package com.example.spending.ui.expenditure;
 
-import android.annotation.SuppressLint;
-import android.graphics.Color;
 import android.util.Log;
 import android.widget.TextView;
 
@@ -12,8 +10,6 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
-
-import org.eazegraph.lib.models.PieModel;
 
 import java.util.HashMap;
 
@@ -91,39 +87,5 @@ public class ExpenditureViewModel extends ViewModel {
 
     public float calculation(float budget, float expenses) {
         return budget - expenses;
-    }
-
-    @SuppressLint("SetTextI18n")
-    public void setData(float tvGroceries_value, float tvFurniture_value, float tvIT_value, float tv_DailyNecessities, float tvOthers, final ExpenditureViewModel)
-    {
-        // Set the data and color to the pie chart
-        pieChart.addPieSlice(
-                new PieModel(
-                        "Groceries",
-                        Integer.parseInt(tvGroceries.getText().toString()),
-                        Color.parseColor("#FFA726")));
-        pieChart.addPieSlice(
-                new PieModel(
-                        "Furniture",
-                        Integer.parseInt(tvFurniture.getText().toString()),
-                        Color.parseColor("#66BB6A")));
-        pieChart.addPieSlice(
-                new PieModel(
-                        "IT",
-                        Integer.parseInt(tvIT.getText().toString()),
-                        Color.parseColor("#EF5350")));
-        pieChart.addPieSlice(
-                new PieModel(
-                        "DailyNecessities",
-                        Integer.parseInt(tvDailyNecessities.getText().toString()),
-                        Color.parseColor("#29B6F6")));
-        pieChart.addPieSlice(
-                new PieModel(
-                        "Others",
-                        Integer.parseInt(tvOthers.getText().toString()),
-                        Color.parseColor("#f629a4")));
-
-        // To animate the pie chart
-        pieChart.startAnimation();
     }
 }
