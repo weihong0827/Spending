3592c14702d0fabf2949bfef709a1af6c23e9384
Clement <clementlohck@outlook.com>
2022-05-29 07:51:52 +0800

    To see if pulling is the same as merging. pie_chart working when run in FragmentView.java

--- app/src/main/java/com/example/spending/MainActivity.java
+++ app/src/main/java/com/example/spending/MainActivity.java
@@ -20,9 +20,6 @@ import org.eazegraph.lib.models.PieModel;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView tvGroceries, tvFurniture, tvIT, tvDailyNecessities, tvOthers;
-    PieChart pieChart;
-
     private ActivityMainBinding binding;
 
     @Override
@@ -41,66 +38,7 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
-
-        // Link those objects with their
-        // respective id's that
-        // we have given in .XML file
-        tvGroceries = findViewById(R.id.tvGroceries);
-        tvFurniture = findViewById(R.id.tvFurniture);
-        tvIT = findViewById(R.id.tvIT);
-        tvDailyNecessities = findViewById(R.id.tvDailyNecessities);
-        tvOthers = findViewById(R.id.tvOthers);
-        pieChart = findViewById(R.id.piechart);
-
-        // Creating a method setData()
-        // to set the text in text view and pie chart
-        setData();
-
     }
-
-    @SuppressLint("SetTextI18n")
-    private void setData()
-    {
-
-        // Set the percentage of language used
-        tvGroceries.setText(Integer.toString(40));
-        tvFurniture.setText(Integer.toString(30));
-        tvIT.setText(Integer.toString(5));
-        tvDailyNecessities.setText(Integer.toString(15));
-        tvOthers.setText(Integer.toString(10));
-
-
-        // Set the data and color to the pie chart
-        pieChart.addPieSlice(
-                new PieModel(
-                        "Groceries",
-                        Integer.parseInt(tvGroceries.getText().toString()),
-                        Color.parseColor("#FFA726")));
-        pieChart.addPieSlice(
-                new PieModel(
-                        "Furniture",
-                        Integer.parseInt(tvFurniture.getText().toString()),
-                        Color.parseColor("#66BB6A")));
-        pieChart.addPieSlice(
-                new PieModel(
-                        "IT",
-                        Integer.parseInt(tvIT.getText().toString()),
-                        Color.parseColor("#EF5350")));
-        pieChart.addPieSlice(
-                new PieModel(
-                        "DailyNecessities",
-                        Integer.parseInt(tvDailyNecessities.getText().toString()),
-                        Color.parseColor("#29B6F6")));
-        pieChart.addPieSlice(
-                new PieModel(
-                        "Others",
-                        Integer.parseInt(tvOthers.getText().toString()),
-                        Color.parseColor("#f629a4")));
-
-        // To animate the pie chart
-        pieChart.startAnimation();
-    }
-
 }
 
 
