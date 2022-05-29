a075911699e886291dd53516e38d6b9e4cd94797
brendalimsh <99495340+brendalimsh@users.noreply.github.com>
2022-05-29 12:15:53 +0800

    created new SelectCategoryFragment

---
+++ app/src/main/java/com/example/spending/ui/select_category/SelectCategoryFragment.java
@@ -0,0 +1,38 @@
+package com.example.spending.ui.select_category;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.spending.R;
+
+public class SelectCategoryFragment extends Fragment {
+
+    private SelectCategoryViewModel mViewModel;
+
+    public static SelectCategoryFragment newInstance() {
+        return new SelectCategoryFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.select_category_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(SelectCategoryViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/example/spending/ui/select_category/SelectCategoryViewModel.java
@@ -0,0 +1,7 @@
+package com.example.spending.ui.select_category;
+
+import androidx.lifecycle.ViewModel;
+
+public class SelectCategoryViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
